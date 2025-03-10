0e0bb169ce2efa5f84129df51fa238560313a1de
Dhanajayan-04 <dhanajayanyasotha@gmail.com>
2025-03-10 22:47:41 +0530

    Completed rotate image by 90 degree

---
+++ Arrays/Medium/Rotate_image_by_90degree.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+void rotate(vector < vector < int >> & matrix) {
+    int n = matrix.size();
+    //transposing the matrix
+    for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+        swap(matrix[i][j], matrix[j][i]);
+    }
+    }
+    //reversing each row of the matrix
+    for (int i = 0; i < n; i++) {
+    reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the square matrix: ";
+    cin >> n;
+
+    vector<vector<int>> arr(n, vector<int>(n));
+
+    cout << "Enter the elements of the matrix row-wise:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    rotate(arr);
+
+    cout << "Rotated Image:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
