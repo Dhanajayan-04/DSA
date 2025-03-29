04735e1df5ba5e9388b6d21fda69e6747dcb5325
Dhanajayan-04 <dhanajayanyasotha@gmail.com>
2025-03-29 21:32:43 +0530

    Completed sum of array elements

--- Arrays/Easy/Sum_of_elements_in_array.cpp
+++ Arrays/Easy/Sum_of_elements_in_array.cpp
@@ -12,13 +12,10 @@ int sumOfArray(int arr[], int n) {
 int main() {
     int n;
     cin >> n;
-
     int arr[n];
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-
     cout << "Sum of array elements: " << sumOfArray(arr, n) << endl;
-
     return 0;
 }
