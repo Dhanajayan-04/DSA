07e396634d2eeea7506efaf2b0c68ebea73406e9
Dhanajayan <dhanajayanyasotha@gmail.com>
2025-05-22 11:45:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
@@ -0,0 +1,19 @@
+// User function template in C++
+
+class Solution {
+  public:
+    // Function to return the count of number of elements in union of two arrays.
+    int findUnion(vector<int>& a, vector<int>& b) {
+        // code here
+        set<int>unionSet;
+        
+        for(int num:a){
+            unionSet.insert(num);
+        }
+        
+        for(int num:b){
+            unionSet.insert(num);
+        }
+        return unionSet.size();
+    }
+};
