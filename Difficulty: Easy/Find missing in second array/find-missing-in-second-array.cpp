4b312de876db4ffd33af873aef3540244f516e83
Dhanajayan <dhanajayanyasotha@gmail.com>
2025-06-09 22:06:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find missing in second array/find-missing-in-second-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+
+  public:
+    vector<int> findMissing(vector<int>& a, vector<int>& b) {
+        // Your code goes here
+        unordered_set<int>setB(b.begin(),b.end());
+        vector<int>result;
+        
+        for(int num:a){
+            if(setB.find(num)==setB.end()){
+                result.push_back(num);
+            }
+        }
+        return result;
+    }
+};
