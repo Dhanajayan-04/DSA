cf7750855d809ffb3a37ec72f3fb83672ebf980a
Dhanajayan <dhanajayanyasotha@gmail.com>
2025-06-19 22:00:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Switch Statement/switch-statement.cpp
@@ -0,0 +1,40 @@
+// User function Template for C++
+
+string utility(int number) {
+
+    // write your code here
+    string result;
+    switch(number){
+        case 1: 
+        result = "One";
+        break;
+        case 2:
+        result = "Two";
+        break;
+        case 3:
+        result = "Three";
+        break;
+        case 4:
+        result = "Four";
+        break;
+        case 5:
+        result = "Five";
+        break;
+        case 6:
+        result = "Six";
+        break;
+        case 7:
+        result = "Seven";
+        break;
+        case 8:
+        result= "Eight";
+        break;
+        case 9:
+        result = "Nine";
+        break;
+        default : 
+        result = "Unknown";
+        break;
+    }
+    return result;
+}
