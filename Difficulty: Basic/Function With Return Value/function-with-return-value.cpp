2e7034a30857d93d7a08b34c25bc5f8f437e4c83
Dhanajayan <dhanajayanyasotha@gmail.com>
2025-06-21 23:15:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With Return Value/function-with-return-value.cpp
@@ -0,0 +1,11 @@
+// User function Template for C++
+
+// Write the complete function returnValueFunction below
+// This function should take n as a parameter
+// return double of n
+int returnValueFunction(int n){
+    int ans;
+    ans=n*2;
+    return ans;
+}
+
