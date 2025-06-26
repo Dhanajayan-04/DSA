b558e124f19d5f318fbb5d9eeab610279d3ba93f
Dhanajayan <dhanajayanyasotha@gmail.com>
2025-06-26 22:04:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print GeeksForGeeks/print-geeks-for-geeks.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Prints Hello World
+    //Hello World//
+
+    // Print GeeksforGeeks
+    cout << "GeeksForGeeks\n";
+
+    return 0;
+}
