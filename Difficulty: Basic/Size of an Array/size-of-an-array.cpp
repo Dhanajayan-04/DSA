7585e83fbc3ab83ec5812489307669755aa976a4
Dhanajayan <dhanajayanyasotha@gmail.com>
2025-06-27 19:13:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Size of an Array/size-of-an-array.cpp
@@ -0,0 +1,8 @@
+// User function Template for C++
+
+// arr[] : declared and initialized above
+
+// calculate the size of the array arr[]
+int size =sizeof(arr)/sizeof(arr[0]);
+cout << size << endl;
+// driver code prints size
