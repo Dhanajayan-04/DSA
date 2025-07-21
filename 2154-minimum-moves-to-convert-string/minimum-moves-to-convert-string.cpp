23e1e8e6a04a285a0861f7e48230d4605480824b
Dhanajayan <dhanajayanyasotha@gmail.com>
2025-07-21 13:17:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (93.73%) - LeetSync

---
+++ 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int moves=0;
+        int i=0;
+        while(i<s.length()){
+            if(s[i]=='X'){
+                moves++;
+                i+=3;
+            }else{
+                i++;
+            }
+        }
+        return moves;
+    }
+};
