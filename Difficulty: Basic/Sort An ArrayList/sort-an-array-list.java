781d46fdeb2629b05d7c6d89e1b806648d07296a
Dhanajayan <dhanajayanyasotha@gmail.com>
2025-09-29 23:31:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort An ArrayList/sort-an-array-list.java
@@ -0,0 +1,7 @@
+class Solution {
+    public static void sortArrayList(ArrayList<Integer> list) {
+        // Your code here
+        Collections.sort(list);
+        // You do not need to print
+    }
+}
